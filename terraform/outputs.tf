028a8e61a825cf5f6bcf2ed22109e9c1c94bdf38
René Sandmann <rene.sandmann@chakademie.org>
2025-09-04 15:30:52 +0200

    outputs hinzugefügt

---
+++ terraform/outputs.tf
@@ -0,0 +1,16 @@
+# Schöne Outputs nach dem Apply.
+
+output "lambda_name" {
+  description = "Name der bereitgestellten Lambda-Funktion"
+  value       = aws_lambda_function.slack_notify.function_name
+}
+
+output "lambda_arn" {
+  description = "ARN der bereitgestellten Lambda-Funktion"
+  value       = aws_lambda_function.slack_notify.arn
+}
+
+# output "lambda_url" {
+#   description = "Öffentliche URL (falls aws_lambda_function_url aktiviert ist)"
+#   value       = aws_lambda_function_url.url.function_url
+# }
